5d7f642b8cae41e53e5b853349343b76dc6165df
WanjohiK <kelvinnyawira2022@gmail.com>
2023-10-20 11:22:58 +0300

    printf

--- main.h
+++ main.h
@@ -1,8 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
+#include <stddef.h>
+#include <string.h>
+/**
+ * struct format - Represents a format specifier and its corresponding function
+ * @id: The format specifier string,  "%s", "%d".
+ * @f: A pointer to the function that handles the format specifier.
+ */
+typedef struct format
+{
+        char *id;
+        int (*f)();
+} match;
+int _putchar(char c);
 int _printf(const char *format, ...);
-int handle_format_specifier(const char **format, va_list args);
+int printf_char(va_list val);
+int print_percent(void);
+int _strlenc(const char *str);
+int _strlen(char *str);
+int printf_integer(va_list args);
+int printf_int(va_list args);
+int print_string(va_list val);
+int printf_binary(va_list args);
+int printf_unsigned(va_list args);
+int printf_octal(va_list args);
+int printf_hex_lower(va_list args);
+int printf_hex_upper(va_list args);
+int printf_string_nonprint(va_list args);
+int printf_pointer(va_list args);
 
-#endif /*MAIN_H*/
+#endif
